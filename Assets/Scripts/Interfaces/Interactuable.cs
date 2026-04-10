fe6e62cc13ebb18821777d3fb73d4da0d15479a9
yolocum25 <myhiro270@gmail.com>
2026-04-10 22:56:25 +0200

    we

--- Assets/Scripts/Interfaces/Interactuable.cs
+++ Assets/Scripts/Interfaces/Interactuable.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public interface IInteractable
 {
-        //una interfaz es un conjunto de funciones abstractas que sirven
-        //como contrato que aquellas clases que implementen la interfaz deben rellenar.
+        
         public void GenerateInteraction();
-        //marco sintecho
+       
     
 }
 
--- Assets/Scripts/Player/MainPlayer.cs
+++ Assets/Scripts/Player/MainPlayer.cs
@@ -1,24 +1,53 @@
-using System;
 using UnityEngine;
 
 public class PlayerMain : MonoBehaviour
 {
     [field: SerializeField] public Transform InteractPoint { get; private set; }
     [field: SerializeField] public float InteractionRadius { get; private set; }
-    
+    [SerializeField] private LayerMask interactionLayer;
+
     public Rigidbody2D Rb { get; private set; }
     public Animator Anim { get; private set; }
-    
+
     private void Awake()
     {
         Rb = GetComponent<Rigidbody2D>();
         Anim = GetComponent<Animator>();
     }
-    //Cuando un jugador nace..
-    // private void Start()
-    // {
-    //     
-    //     transform.position = GameManager.Instance.SavedPosition;
-    //     transform.eulerAngles = GameManager.Instance.SavedOrientation;
-    // }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PerformInteraction();
+        }
+    }
+
+    private void PerformInteraction()
+    {
+       
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(InteractPoint.position, InteractionRadius, interactionLayer);
+
+        foreach (var col in colliders)
+        {
+            // Intentamos obtener la interfaz del objeto tocado
+            IInteractable interactable = col.GetComponent<IInteractable>();
+            
+            if (interactable != null)
+            {
+                interactable.GenerateInteraction();
+                break; 
+            }
+        }
+    }
+
+    
+    private void OnDrawGizmosSelected()
+    {
+        if (InteractPoint != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(InteractPoint.position, InteractionRadius);
+        }
+    }
 }
